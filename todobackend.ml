17871cd90349a17c7d3840036c7b1515fddafe2b
Arnout Engelen <arnouten@bzzt.net>
2015-05-08 10:46:09 +0200

    Marshalling/unmarshalling of todo's, add default value for 'completed'

--- todobackend.ml
+++ todobackend.ml
@@ -1,11 +1,7 @@
 open Core_kernel.Std
 open Opium.Std
 
-module TodoStorage = Caml.Map.Make(Int)
-
 Random.self_init()
-
-let stored_todos = ref TodoStorage.empty
 
 let add_cors_headers (headers: Cohttp.Header.t): Cohttp.Header.t =
   Cohttp.Header.add_list headers [
@@ -29,27 +25,45 @@ type todo = {
   completed: bool;
 }
 
-let json_of_todos todos = 
-  `A todos
+module TodoStorage = Caml.Map.Make(Int)
+let stored_todos = ref TodoStorage.empty
 
-let json_of_todo { title ; completed }: Ezjsonm.value =
+let get_or_else t path default =
+  let open Ezjsonm in
+    if (mem t path)
+    then (find t path)
+    else default
+
+let todo_of_json json =
+  let open Ezjsonm in
+  {
+    title = get_string (find json ["title"]);
+    completed = get_bool (get_or_else json ["completed"] (bool false))
+  }
+
+let todo_of_json json = todo_of_json (Ezjsonm.value json)
+
+let json_of_todo { title ; completed }: Ezjsonm.t =
   let open Ezjsonm in
   dict [ "title", (string title)
        ; "completed", (bool completed) ]
 
-let print_param = put "/hello/:name" begin fun req ->
-  `String ("Hello " ^ param req "name") |> respond'
-end
+let json_of_todos (todos: todo list): Ezjsonm.t =
+  `A (List.map todos ~f:(fun json -> Ezjsonm.value (json_of_todo json)))
 
 let get_todos = get "/todos" begin fun _ ->
   let todos = TodoStorage.fold (fun _ value acc -> value :: acc) !stored_todos [];
   in
-    `Json (todos |> json_of_todos) |> respond'
+    `Json (json_of_todos todos)
+    |> respond'
 end
 
 let post_todos = post "/todos" begin fun request ->
   App.json_of_body_exn request 
-  >>| fun json -> stored_todos := TodoStorage.add (Random.int 100000) (Ezjsonm.value json) !stored_todos; `Json json
+  >>| fun json ->
+    let todo = todo_of_json json in
+      stored_todos := TodoStorage.add (Random.int 100000) todo !stored_todos;
+      `Json (json_of_todo todo)
   |> respond
 end
 
